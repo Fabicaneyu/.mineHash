0616ac6f68894366e3fa715d8c00ddfa71377628
shepard-2209 <it-gimenes@hotmail.com>
2019-10-06 16:35:30 -0300

    comentários na aplicacao oshi/ melhora de código

--- Aplicação/Prototipo/prototipo-aplicacao/src/main/java/com/mycompany/prototipo/aplicacao/Dados.java
+++ Aplicação/Prototipo/prototipo-aplicacao/src/main/java/com/mycompany/prototipo/aplicacao/Dados.java
@@ -2,19 +2,40 @@ package com.mycompany.prototipo.aplicacao;
 
 public class Dados {
 
-    PrototipoAplicacao dadosApp = new PrototipoAplicacao();
-    ConexaoBanco insert = new ConexaoBanco();
-//    private String nome = "Marcelo";
-//    private String sobrenome = "Lixo";
+    // vamos instanciar duas classes que serão necessárias para executar o envio
+    // de dados ao banco
+    
+    CapturaDadosOshi dadosAplicacao = new CapturaDadosOshi();
+    ConexaoBanco conexaoBanco = new ConexaoBanco();
 
-    public void enviarBanco() {
-        insert.getConnection();
-        insert.template().update(
+    public void enviarDados() {
+        
+        // aqui chamamos o método de montagem de conexão ao banco que criamos
+        // na classe ConexaoBanco :)
+        conexaoBanco.montarConexao();
+        
+        // agora aplicamos o método vindo direto do JDBC para a construção da 
+        // nossa query
+        
+        // independente do que você quer fazer, seja select ou insert, o método
+        // sempre será ****.template().update(), então cuidado pra não confundir :)
+        
+        // e como pode ser visto abaixo, vamos usar a instância que criamos (dadosAplicacao)
+        // você pode ver que não podemos inserir os dados que queremos direto na query,
+        // então temos que fazer concatenação
+        
+        // no insert temos que mencionar os campos que iremos realizar a inserção
+        // e após "values", temos que colocar "?" para cada campo mencionado
+        
+        // depois, iremos colocar os dados que queremos e que irão substituir os "?"
+        // LEMBRE-SE DE COLOCAR OS DADOS NA ORDEM QUE VOCÊ COLOCOU OS CAMPOS
+        
+        conexaoBanco.template().update(
                 "insert into Computador (hostname,fabricante,modelo,processador,memoriaTotal,memoriaDisp,"
                 + "sistemaOperacional) values (?,?,?,?,?,?,?)",
-                dadosApp.hostname(), dadosApp.fabricante(), dadosApp.modelo(),
-                dadosApp.processador(), dadosApp.memoriaTotal(), dadosApp.memoriaDisp(),
-                dadosApp.sistemaOperacional());
+                dadosAplicacao.getHostname(), dadosAplicacao.getFabricante(), dadosAplicacao.getModelo(),
+                dadosAplicacao.getProcessador(), dadosAplicacao.getMemoriaTotal(), dadosAplicacao.getMemoriaDisp(),
+                dadosAplicacao.getSistemaOperacional());
 
     }
 }
