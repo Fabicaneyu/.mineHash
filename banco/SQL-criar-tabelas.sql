5a9ff89dd30a3cdaea3b68841903205c8b916fe4
shepard-2209 <it-gimenes@hotmail.com>
2019-10-15 15:02:47 -0300

    banco

---
+++ banco/SQL-criar-tabelas.sql
@@ -0,0 +1,59 @@
+CREATE TABLE tb_minerador (
+  id_minerador INT IDENTITY(1,1) PRIMARY KEY,
+  nome VARCHAR(255) NOT NULL,
+  data_nasc DATE NOT NULL,
+  telefone NVARCHAR(2) NOT NULL,
+  -- Endereço
+  endereco_numero INT NOT NULL,
+  cep CHAR(8),
+  logradouro VARCHAR(16) NOT NULL,
+  endereco_nome VARCHAR(255) NOT NULL,
+  endereco_numero VARCHAR(65) NOT NULL,
+  endereco_cidade VARCHAR(65) NOT NULL,
+  endereco_bairro VARCHAR(65) NOT NULL,
+<<<<<<< HEAD
+  endereco as (
+    logradouro + ' ' + endereco_nome + ', ' + endereco_numero + ', '
+    + endereco_bairro + ', ' + endereco_cidade + ' - ' + endereco_cep
+  ) -- Ex: Rua Edimar Fagundes Filho, 5023, São Paulo - 05232-792
+=======
+  -- endereco as (
+  --   endereco_logradouro + ' ' + endereco_nome + ', ' + endereco_numero + ', '
+  --   + endereco_bairro + ', ' + endereco_cidade + ' - ' + endereco_cep
+  -- ) -- Ex: Rua Edimar Fagundes Filho, 5023, São Paulo - 05232-792
+>>>>>>> 93b4432d010104804389a1c32f4641a92b314b1d
+);
+
+CREATE TABLE tb_computador (
+  id_computador INT PRIMARY KEY IDENTITY(1000, 1),
+  fk_minerador INT NOT NULL,
+  nm_disco VARCHAR(255) NOT NULL,
+  nm_ram VARCHAR(255) NOT NULL,
+  nm_so VARCHAR(255) NOT NULL,
+  nm_gpu VARCHAR(255),
+  nm_hostname VARCHAR(255) NOT NULL,
+  nm_cpu VARCHAR(255) NOT NULL,
+  nm_modelo VARCHAR(255),
+  nm_processador VARCHAR(255) NOT NULL,
+  nm_fabricante VARCHAR(255),
+  FOREIGN KEY (fk_minerador) REFERENCES tb_minerador (id_minerador)
+);
+
+CREATE TABLE tb_processo (
+  id_processo INT PRIMARY KEY IDENTITY(100000, 1),
+  nu_pid INT NOT NULL,
+  nm_processo VARCHAR(255)
+);
+
+CREATE TABLE tb_desempenho (
+  id_desempenho INT PRIMARY KEY IDENTITY(50000, 1),
+  fk_computador INT NOT NULL,
+  fk_processo INT NOT NULL, --Questionar dps...
+  nu_cpu NUMERIC NOT NULL,
+  nu_ram NUMERIC NOT NULL,
+  nu_disco NUMERIC NOT NULL,
+  nu_gpu NUMERIC NOT NULL,
+  nu_temperatura NUMERIC NOT NULL,
+  FOREIGN KEY (fk_computador) REFERENCES tb_computador (id_computador),
+  FOREIGN KEY (fk_processo) REFERENCES tb_processo (id_processo)
+);
